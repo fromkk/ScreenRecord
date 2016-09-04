c9530195615bed9e96cf20f9ac52f8fa674d9416
fromkk <info@fromkk.info>
2016-09-04 15:10:23 +0900

    Initial Commit

---
+++ ScreenRecordSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScreenRecordSample
+//
+//  Created by Ueoka Kazuya on 2016/09/04.
+//  Copyright © 2016年 fromKK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
